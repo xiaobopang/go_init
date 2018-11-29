3a232761d5af1b015d9a10fa487759dbdf0ff654
pangxiaobo <10846295@qq.com>
2018-11-29 16:01:37 +0800

    update

---
+++ helpers/common.go
@@ -0,0 +1,33 @@
+package helpers
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"net"
+)
+
+//md5加密
+func EncodeMD5(value string) string {
+	m := md5.New()
+	m.Write([]byte(value))
+
+	return hex.EncodeToString(m.Sum(nil))
+}
+
+// 获取服务器IP
+func GetLocalIp() string {
+	addrSlice, err := net.InterfaceAddrs()
+	if err != nil {
+		fmt.Println("Get local IP addr failed!")
+		return "localhost"
+	}
+	for _, addr := range addrSlice {
+		if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if nil != ipnet.IP.To4() {
+				return ipnet.IP.String()
+			}
+		}
+	}
+	return "localhost"
+}
