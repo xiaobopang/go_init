a5a12d45dd8d96bef5f5bcb43a6b977cbd3b111b
pangxiaobo <10846295@qq.com>
2018-11-29 11:57:55 +0800

    websocket

--- controllers/ws.go
+++ controllers/ws.go
@@ -6,7 +6,7 @@ package controllers
  * @Email: 10846295@qq.com
  * @Create At: 2018-11-29 10:36:11
  * @Last Modified By: pangxiaobo
- * @Last Modified At: 2018-11-29 11:09:15
+ * @Last Modified At: 2018-11-29 11:57:33
  * @Description: This is description.
  */
 
@@ -29,6 +29,16 @@ var wsupgrader = websocket.Upgrader{
 		return true
 	},
 }
+var messageChan = make(chan string)
+
+//此处为服务端接收 需要推送的信息
+func updateMsg() {
+	for {
+		var messageBuf string
+		fmt.Scanln(&messageBuf)
+		messageChan <- messageBuf
+	}
+}
 
 func (ws *WsController) WsHandler(w http.ResponseWriter, r *http.Request) {
 	conn, err := wsupgrader.Upgrade(w, r, nil)
@@ -36,6 +46,8 @@ func (ws *WsController) WsHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Could not open websocket connection", http.StatusBadRequest)
 		return
 	}
+
+	defer conn.Close()
 	go echo(conn)
 }
 
@@ -46,18 +58,28 @@ func echo(conn *websocket.Conn) {
 			fmt.Println(err)
 			return
 		}
-		if string(msg) == "ping" {
+		switch string(msg) {
+		case "ping":
 			fmt.Println("ping")
 			time.Sleep(time.Second * 2)
-			err = conn.WriteMessage(msgType, []byte("pong"))
+			err = conn.WriteMessage(msgType, []byte("test"))
 			if err != nil {
 				fmt.Println(err)
 				return
 			}
-		} else {
+		case "test":
+			fmt.Println("test")
+			time.Sleep(time.Second * 2)
+			err = conn.WriteMessage(msgType, []byte(<-messageChan))
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+		default:
 			conn.Close()
 			fmt.Println(string(msg))
 			return
 		}
+
 	}
 }
