b0f05653ccd99e0edf77d07f64a8f4a7f8579bd3
pangxiaobo <10846295@qq.com>
2019-05-09 11:15:48 +0800

    add some common func

--- go.mod
+++ go.mod
@@ -1,16 +1,15 @@
 module github.com/xiaobopang/go_init
 
 require (
-	github.com/EDDYCJY/go-gin-example v0.0.0-20190202122259-3c09cf4acfea // indirect
-	github.com/astaxie/beego v1.11.1
+	cloud.google.com/go v0.37.4 // indirect
 	github.com/denisenkom/go-mssqldb v0.0.0-20190204142019-df6d76eb9289 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/erikstmartin/go-testdb v0.0.0-20160219214506-8d10e4a1bae5 // indirect
 	github.com/gin-contrib/sse v0.0.0-20190125020943-a7658810eb74 // indirect
 	github.com/gin-gonic/gin v1.3.0
 	github.com/go-ini/ini v1.42.0
+	github.com/go-sql-driver/mysql v1.4.1 // indirect
 	github.com/gofrs/uuid v3.2.0+incompatible // indirect
-	github.com/golang/protobuf v1.2.0 // indirect
 	github.com/gomodule/redigo v2.0.0+incompatible
 	github.com/gopherjs/gopherjs v0.0.0-20181103185306-d547d1d9531e // indirect
 	github.com/gorilla/websocket v1.4.0
@@ -19,7 +18,9 @@ require (
 	github.com/jinzhu/now v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.5 // indirect
 	github.com/jtolds/gls v4.2.1+incompatible // indirect
+	github.com/lib/pq v1.0.0 // indirect
 	github.com/mattn/go-isatty v0.0.4 // indirect
+	github.com/mattn/go-sqlite3 v1.10.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/smartystreets/assertions v0.0.0-20190215210624-980c5ac6f3ac // indirect
@@ -27,10 +28,14 @@ require (
 	github.com/streadway/amqp v0.0.0-20190214183023-884228600bc9
 	github.com/stretchr/testify v1.3.0 // indirect
 	github.com/ugorji/go/codec v0.0.0-20190204201341-e444a5086c43 // indirect
+	golang.org/x/sync v0.0.0-20190412183630-56d357773e84 // indirect
+	golang.org/x/sys v0.0.0-20190416152802-12500544f89f // indirect
+	google.golang.org/appengine v1.5.0 // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
 	gopkg.in/ini.v1 v1.42.0 // indirect
 	gopkg.in/yaml.v2 v2.2.2 // indirect
+	qiniupkg.com/x v7.0.8+incompatible
 )
 
 replace (
--- helper/common.go
+++ helper/common.go
@@ -9,19 +9,126 @@ package helper
  */
 
 import (
+	"bytes"
 	"crypto/md5"
-	"encoding/hex"
+	"crypto/sha256"
+	"encoding/base64"
 	"fmt"
+	"math/rand"
 	"net"
 	"regexp"
+	"strconv"
+	"strings"
+	"time"
+
+	"qiniupkg.com/x/log.v7"
 )
 
-//md5加密
-func EncodeMD5(value string) string {
-	m := md5.New()
-	m.Write([]byte(value))
+const letterBytes = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const numLetterBytes = "0123456789"
 
-	return hex.EncodeToString(m.Sum(nil))
+const (
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+)
+
+var src = rand.NewSource(time.Now().UnixNano())
+
+func Md5(str string) string {
+	if str == "" {
+		return ""
+	}
+	init := md5.New()
+	init.Write([]byte(str))
+	return fmt.Sprintf("%x", init.Sum(nil))
+}
+
+func Sha256(s string) string {
+	if s == "" {
+		return ""
+	}
+	h := sha256.New()
+	h.Write([]byte(s))
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
+
+func Base64(input string) string {
+	return base64.StdEncoding.EncodeToString([]byte(input))
+}
+
+// 字符串转换整型
+func IpString2Int(ipstring string) int64 {
+	ipSegs := strings.Split(ipstring, ".")
+	ipInt := 0
+	var pos uint = 24
+	for _, ipSeg := range ipSegs {
+		tempInt, _ := strconv.Atoi(ipSeg)
+		tempInt = tempInt << pos
+		ipInt = ipInt | tempInt
+		pos -= 8
+	}
+	return int64(ipInt)
+}
+
+// 整型转换成字符串
+func IpInt2String(ipInt int) string {
+	ipSegs := make([]string, 4)
+	var len int = len(ipSegs)
+	buffer := bytes.NewBufferString("")
+	for i := 0; i < len; i++ {
+		tempInt := ipInt & 0xFF
+		ipSegs[len-i-1] = strconv.Itoa(tempInt)
+		ipInt = ipInt >> 8
+	}
+	for i := 0; i < len; i++ {
+		buffer.WriteString(ipSegs[i])
+		if i < len-1 {
+			buffer.WriteString(".")
+		}
+	}
+	return buffer.String()
+}
+
+func randString(n int, LetterBytes string) string {
+	b := make([]byte, n)
+	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(LetterBytes) {
+			b[i] = LetterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+	return string(b)
+}
+
+func RandString(n int) string {
+	return randString(n, letterBytes)
+}
+
+func RandNumString(n int) string {
+	return randString(n, numLetterBytes)
+}
+
+//生成以日期随机的id
+func GenDateRandId() string {
+	return time.Now().Format("20060102150405") + RandNumString(6)
+}
+
+var Rander = rand.New(src)
+
+// 随机数生成
+// @Param	min 	int	最小值
+// @Param 	max		int	最大值
+// @return  int
+func RandInt(min int, max int) int {
+	num := Rander.Intn((max - min)) + min
+	return num
 }
 
 // 获取服务器IP
@@ -29,7 +136,7 @@ func GetLocalIp() string {
 	addrSlice, err := net.InterfaceAddrs()
 	if err != nil {
 		fmt.Println("Get local IP addr failed!")
-		return "localhost"
+		return "127.0.0.1"
 	}
 	for _, addr := range addrSlice {
 		if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
@@ -38,7 +145,7 @@ func GetLocalIp() string {
 			}
 		}
 	}
-	return "localhost"
+	return "127.0.0.1"
 }
 
 // 是否是email
@@ -48,4 +155,53 @@ func IsEmail(email string) bool {
 	}
 	ok, _ := regexp.MatchString(`^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[0-9a-zA-Z]{2,3}$`, email)
 	return ok
+}
+
+//时间转时间戳
+func DateTimeToTimestamp(datetime string) int64 {
+	loc, _ := time.LoadLocation("Asia/Shanghai") //设置时区
+	tt, err := time.ParseInLocation("2006-01-02 15:04:05", datetime, loc)
+	if err != nil {
+		log.Error("datetime to timestamp error: ", err)
+		return 0
+	}
+	return tt.Unix()
+}
+
+//时间转时间戳
+func DateToTimestamp(datetime string) int64 {
+	loc, _ := time.LoadLocation("Asia/Shanghai") //设置时区
+	tt, err := time.ParseInLocation("2006-01-02", datetime, loc)
+	if err != nil {
+		log.Error("date to timestamp error: ", err)
+		return 0
+	}
+	return tt.Unix()
+}
+
+//时间戳 to 时间
+func TimestampToDate(timestamp int64) string {
+	return time.Unix(timestamp, 0).Format("2006-01-02 15:04:05")
+}
+
+//当前时间
+func NowDatetime() string {
+	return time.Now().Format("2006-01-02 15:04:05")
+}
+
+// 当前时间戳
+func NowTimestamp() int64 {
+	return time.Now().Unix()
+}
+
+// implode 类似php的 implode函数,将数组转为字符串
+func Implode(originArr []string, symbol string) string {
+	newStr := strings.Join(originArr, symbol)
+	return newStr
+}
+
+//explode 类似php的 explode函数，将字符串转为数组
+func Explode(originStr string, symbol string) []string {
+	strArr := strings.Split(originStr, symbol)
+	return strArr
 }
